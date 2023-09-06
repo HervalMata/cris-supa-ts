26cb5a03a439d40608ff9f767abe67b590fdd209
HervalMata <hervalmataamparo@gmail.com>
2023-09-06 16:50:37 -0300

    chores: cris supabase - setting-up main layout component

---
+++ src/app/layout/MainLayout.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+// @ts-ignore
+export default function MainLayout({ children }) {
+    return (
+        <>
+            <div id="main_layout" className="min-w-[1056px] max-w-[1300px] mx-auto border">
+                <div></div>
+            </div>
+        </>
+    );
+}
