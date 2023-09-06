9f338885dbcb2815aea49a5d04b816c94e7087ef
HervalMata <hervalmataamparo@gmail.com>
2023-09-06 20:00:07 -0300

    chores: cris supabase - setting-up top menu component

--- src/app/layout/MainLayout.tsx
+++ src/app/layout/MainLayout.tsx
@@ -1,11 +1,15 @@
 "use client"
 
+import TopMenu from "@/app/layout/includes/TopMenu";
+
 // @ts-ignore
 export default function MainLayout({ children }) {
     return (
         <>
             <div id="main_layout" className="min-w-[1056px] max-w-[1300px] mx-auto border">
-                <div></div>
+                <div>
+                    <TopMenu />
+                </div>
             </div>
         </>
     );
---
+++ src/app/layout/includes/TopMenu.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import {BsChevronDown} from "react-icons/bs";
+import {AiOutlineShoppingCart} from "react-icons/ai";
+
+export default function TopMenu() {
+    return (
+        <>
+            <div id="top_menu" className="border-b">
+                <div className="flex items-center justify-between w-full mx-auto max-w-[1200px]">
+                    <ul id="top_menu_left" className="flex items-center px-2 h-8 text-[11px] text-[#333]">
+                        <li className="relative px-3">
+                            <Link href="/auth" className="flex items-center gap-2 hover:underline cursor-pointer">
+                                <div>Entre</div>
+                                <BsChevronDown />
+                            </Link>
+                            <div id="auth_drop_down" className="absolute bg-white w-[200px] text-[#333] z-40 top-[20px] left-0 border shadow-lg">
+                                <div className="flex items-center justify-start gap-1 p-3">
+                                    <img width={50} src="https://picsum.photos/200/" alt="demo" />
+                                    <p className="font-bold text-[13px]">John Weeks</p>
+                                </div>
+                                <div className="border-b" />
+                                <ul className="bg-white">
+                                    <li className="text-[11px] py-2 px-4 hover:underline text-pink-400 hover:text-pink-400 cursor-pointer">
+                                        Sair
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className="px-3 hover:underline cursor-pointer">
+                            Vendas Diárias
+                        </li>
+                        <li className="px-3 hover:underline cursor-pointer">
+                            Ajuda e Contato
+                        </li>
+                    </ul>
+                    <ul id="top_menu_right" className="flex items-center px-2 h-8 text-[11px] text-[#333]">
+                        <li className="flex items-center gap-2 px-3 hover:underline cursor-pointer">
+                            <img width={32} src="/images/br.png" alt="br" />
+                            Entregas Para
+                        </li>
+                        <li className="px-3 hover:underline cursor-pointer">
+                            <div className="relative">
+                                <AiOutlineShoppingCart />
+                                <div className="absolute text-[10px] -top-[5px] -right-[5px] bg-pink-400 w--[14px] h-[14px] rounded-full text-white">
+                                    <div className="flex items-center justify-center -mt-[1px]">
+                                        3
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
