cfbf3957aa2f9467856e783876ec5501ea8d3d52
HervalMata <hervalmataamparo@gmail.com>
2023-09-07 00:42:24 -0300

    chores: cris supabase - setting-up footer comÃponent

--- src/app/layout/MainLayout.tsx
+++ src/app/layout/MainLayout.tsx
@@ -2,17 +2,28 @@
 
 import TopMenu from "@/app/layout/includes/TopMenu";
 import SubMenu from "@/app/layout/includes/SubMenu";
+import React from "react";
+import MainHeader from "@/app/layout/includes/MainHeader";
+import Footer from "@/app/layout/includes/Footer";
 
-// @ts-ignore
-export default function MainLayout({ children }) {
+interface IProps {
+    children: any;
+}
+
+const MainLayout: React.FC<IProps> = ({ children }) => {
     return (
         <>
             <div id="main_layout" className="min-w-[1056px] max-w-[1300px] mx-auto border">
                 <div>
                     <TopMenu />
+                    <MainHeader />
                     <SubMenu />
+                    {children}
+                    <Footer />
                 </div>
             </div>
         </>
     );
 }
+
+export default MainLayout;
---
+++ src/app/layout/includes/Footer.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+export default function Footer() {
+    return (
+        <>
+            <div id="footer" className="border-t mt-20 px-2">
+                <div className="flex items-baseline justify-between w-full mx-auto max-w-[1200px] py-10">
+                    <ul className="text-gray-700">
+                        <li className="font-bold text-lg">Compre</li>
+                        <li className="mt-2 py-1 text-xs hover:underline cursor-pointer">Cadastro</li>
+                        <li className="py-1 text-xs hover:underline cursor-pointer">Ajuda</li>
+                        <li className="py-1 text-xs hover:underline cursor-pointer">Loja</li>
+                    </ul>
+                    <ul className="text-gray-700">
+                        <li className="font-bold text-lg">Compre</li>
+                        <li className="mt-2 py-1 text-xs hover:underline cursor-pointer">Cadastro</li>
+                        <li className="py-1 text-xs hover:underline cursor-pointer">Ajuda</li>
+                        <li className="py-1 text-xs hover:underline cursor-pointer">Loja</li>
+                    </ul>
+                    <ul className="text-gray-700">
+                        <li className="font-bold text-lg">Compre</li>
+                        <li className="mt-2 py-1 text-xs hover:underline cursor-pointer">Cadastro</li>
+                        <li className="py-1 text-xs hover:underline cursor-pointer">Ajuda</li>
+                        <li className="py-1 text-xs hover:underline cursor-pointer">Loja</li>
+                    </ul>
+                    <ul className="text-gray-700">
+                        <li className="font-bold text-lg">Compre</li>
+                        <li className="mt-2 py-1 text-xs hover:underline cursor-pointer">Cadastro</li>
+                        <li className="py-1 text-xs hover:underline cursor-pointer">Ajuda</li>
+                        <li className="py-1 text-xs hover:underline cursor-pointer">Loja</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
